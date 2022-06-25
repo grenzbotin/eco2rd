f7e18780ff1441fcc918e958ee84c2a8256be8b1
grenzbotin <grenzbotin@googlemail.com>
2022-06-25 11:57:36 +0200

    improve mockdata representation for historical data, update dep

--- src/hooks/historicalMock.ts
+++ src/hooks/historicalMock.ts
@@ -1,14 +1,23 @@
-const historicalMock = {
-  2021: {
-    4: { green: 2321232, total: 23423423 },
-    5: { green: 528381981, total: 17876813146 },
-    6: { green: 2322323, total: 2343232 }
-  },
-  2022: {
-    4: { green: 2321203, total: 23423499900 },
-    5: { green: 528381981, total: 17876813146 },
-    6: { green: 23242302, total: 234320392 }
+function generateRandomNumber(maxLimit = 27876813146) {
+  const rand = Math.random() * maxLimit;
+  return Math.floor(rand);
+}
+
+const generateYear = (months: number) => {
+  const year = {};
+
+  for (let i = 0; i < months; i++) {
+    const total = generateRandomNumber();
+    const green = generateRandomNumber(total);
+    year[i] = { green, total };
   }
+
+  return year;
+};
+
+const historicalMock = {
+  2021: generateYear(12),
+  2022: generateYear(6)
 };
 
 export default historicalMock;
