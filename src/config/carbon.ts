ab6fffa08234b320e44a376cd7b465abb89dacaa
grenzbotin <grenzbotin@googlemail.com>
2022-06-25 19:24:10 +0200

    update co2eq numbers for various countries (2020)

--- src/config/carbon.ts
+++ src/config/carbon.ts
@@ -1,63 +1,5 @@
 // -----------------------------------------------------------------
-// SOURCES
-//
-// EUROPE, 2020
-// https://www.eea.europa.eu/data-and-maps/daviz/co2-emission-intensity-9#tab-googlechartid_googlechartid_googlechartid_chart_1111
-//
-// UK, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_UK.pdf
-//
-// US, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_USA.pdf
-//
-// China, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_China.pdf
-//
-// World, 2019
-// https://www.iea.org/reports/global-energy-co2-status-report-2019/emissions
-//
-// Renewable default
-// https://gitlab.com/wholegrain/carbon-api-2-0/-/blob/master/includes/carbonapi.php
-//
-// Canada, 2015/2018
-// https://www.statista.com/statistics/917172/emission-intensity-canada-by-province/
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Canada.pdf
-//
-// Australia
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Australia.pdf
-//
-// Argentina 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Argentina.pdf
-//
-// Brazil, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Brazil.pdf
-//
-// India, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_India.pdf
-//
-// Indonesia, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Indonesia.pdf
-//
-// Japan, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Japan.pdf
-//
-// Mexico, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Mexico.pdf
-//
-// Russia, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Russia.pdf
-//
-// Saudi Arabia, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_SaudiArabia.pdf
-//
-// South Africa, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_South_Africa.pdf
-//
-// South Korea, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_South_Korea.pdf
-//
-// TURKEY, 2018
-// https://www.climate-transparency.org/wp-content/uploads/2019/11/B2G_2019_Turkey.pdf
+// SOURCES: See Readme
 // -----------------------------------------------------------------
 
 export const CO2_PER_KWH_GRID_REGION = {
@@ -89,23 +31,23 @@ export const CO2_PER_KWH_GRID_REGION = {
   SK: 101.7,
   FI: 68.6,
   SE: 8.8,
-  UK: 222.0,
-  AUSTRALIA: 717,
-  ARGENTINA: 313,
-  BRAZIL: 74,
-  CANADA: 140,
-  INDIA: 708,
-  INDONESIA: 761,
-  JAPAN: 506,
-  MEXICO: 449,
-  RUSSIA: 325,
-  SAUDI_ARABIA: 732,
-  SOUTH_AFRICA: 928,
-  SOUTH_KOREA: 500,
-  TURKEY: 481,
+  UK: 176.1,
+  AUSTRALIA: 667,
+  ARGENTINA: 307,
+  BRAZIL: 61.7,
+  CANADA: 69.3,
+  INDIA: 708.2,
+  INDONESIA: 717.7,
+  JAPAN: 465.8,
+  MEXICO: 431.4,
+  RUSSIA: 310.2,
+  SAUDI_ARABIA: 505.9,
+  SOUTH_AFRICA: 900.6,
+  SOUTH_KOREA: 415.6,
+  TURKEY: 375,
   EU_DEFAULT: 230.7,
-  US_DEFAULT: 401,
-  CHINA_DEFAULT: 555,
+  US_DEFAULT: 342.8,
+  CHINA_DEFAULT: 537.4,
   WORLD_DEFAULT: 475
 };
 
@@ -117,33 +59,6 @@ export const CO2_PER_KWH_GRID_REGION = {
 const CO2_GRAMM_PER_KM_CONVENTIONAL_CAR = {
   nsb: 165,
   csb: 258
-};
-
-const CO2_GRAMM_PER_KM_ELECTIC_CAR = {
-  EU_DEFAULT: {
-    nsb: 40,
-    csb: 109
-  },
-  FR: {
-    nsb: 7,
-    csb: 72
-  },
-  DE: {
-    nsb: 59,
-    csb: 130
-  },
-  NL: {
-    nsb: 50,
-    csb: 120
-  },
-  UK: {
-    nsb: 26,
-    csb: 94
-  },
-  US_DEFAULT: {
-    nsb: 59,
-    csb: 130
-  }
 };
 
 // -----------------------------------------------------------------
@@ -161,7 +76,6 @@ export const utils = {
   CO2_PER_KWH_RENEWABLE: 33.4,
   CO2_GRAMS_TO_LITRES: 0.5562,
   CO2_GRAMM_PER_KM_CONVENTIONAL_CAR,
-  CO2_GRAMM_PER_KM_ELECTIC_CAR,
   KWH_SMARTPHONE_CHARGED,
   KWH_1L_WATER_BOILED,
   CO2_GRAMM_TREE_ABSORBATION_PER_DAY,
